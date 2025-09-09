f36255f5cbd2643fdb02e53d9e74bb492c04339c
Hang Fan <fanhang995@gmail.com>
2025-09-09 20:16:21 +1000

    page update

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
     <>
         <h1 style= {{ textAlign: "center", color: "blue" }}>Nextjs Demo</h1>
         <hr />	
-        <h2 style={{ textAlign: "center", color: "red" }}>Owen won 1 billion Lotto</h2>
+        <h2 style={{ textAlign: "center", color: "red" }}>Owen won 10 million Lotto on 13/09/2025 </h2>
     </>
   );
 }
